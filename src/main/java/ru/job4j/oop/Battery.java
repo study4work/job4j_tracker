1694e1fdd28c2d25faef714e02756878242494c5
study4work <FaJlJleN@ya.ru>
2020-10-31 20:33:21 +0300

    1.7. Состояние объекта

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -8,7 +8,7 @@ public class Battery {
     }
 
     public void exchange(Battery another) {
-        another.load = another.load - this.load;
+        another.load = another.load + this.load;
         this.load = 0;
     }
 
