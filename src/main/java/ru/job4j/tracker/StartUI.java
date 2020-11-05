dfdacda3cea1f8b69287678d9778c626547c9eb3
study4work <FaJlJleN@ya.ru>
2020-11-05 23:57:32 +0300

    2. Перегрузка конструктора

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -8,5 +8,8 @@ public class StartUI {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
         String currentDateTimeFormat = item.getTime().format(formatter);
         System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
+
+        Item userFirst = new Item(1,"Alex");
+        Item userSecond = new Item(2);
     }
 }
