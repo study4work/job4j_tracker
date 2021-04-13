a2c850e3c9d4d57e87610e668900cc276dfae342
study4work <FaJlJleN@ya.ru>
2021-04-13 21:07:00 +0300

    2. Идентичные тексты

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,22 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = false;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String originString : origin) {
+            check.add(originString);
+            for (String textString : text) {
+                if(check.contains(textString)) {
+                    rsl = true;
+                }
+                break;
+            }
+        }
+        return rsl;
+    }
+}
