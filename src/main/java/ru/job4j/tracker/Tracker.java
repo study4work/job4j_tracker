265e935e1d62806f6586f3ac47ed2fdce61776db
study4work <FaJlJleN@ya.ru>
2021-04-13 19:59:31 +0300

    6. Изменить программу Tracker из 2-го модуля

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,14 +1,11 @@
 package ru.job4j.tracker;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public final class Tracker {
-//    private final Item[] items = new Item[100];
     private final List<Item> items = new ArrayList<>();
     private int ids = 1;
-    private int size = 0;
 
     private static Tracker excess = null;
 
@@ -24,40 +21,22 @@ public final class Tracker {
         items.add(item);
         return item;
     }
-//    public Item add(Item item) {
-//        item.setId(ids++);
-//        items[size++] = item;
-//        return item;
-//    }
 
     private int indexOf(int id) {
         int rsl = -1;
-        for(Item item : items) {
-            if(item.getId() == id) {
-                rsl = items.indexOf(item);
+        for(int i = 0; i < items.size(); i++) {
+            if(items.get(i).equals(id)) {
+                rsl = i;
                 break;
             }
         }
         return rsl;
     }
-//    private int indexOf(int id) {
-//        int rsl = -1;
-//        for (int index = 0; index < size; index++) {
-//            if (items[index].getId() == id) {
-//                rsl = index;
-//                break;
-//            }
-//        }
-//        return rsl;
-//    }
+
       public Item findById(int id) {
           int index = indexOf(id);
           return index != -1 ? items.get(index) : null;
       }
-//    public Item findById(int id) {
-//        int index = indexOf(id);
-//        return index != -1 ? items[index] : null;
-//    }
 
     public List<Item> findAll(){
         List<Item> item = new ArrayList<>();
@@ -68,17 +47,7 @@ public final class Tracker {
         }
         return item;
     }
-//    public Item[] findAll() {
-//        Item[] item = new Item[items.length];
-//        int size = 0;
-//        for (int i = 0; i < items.length; i++) {
-//            if (items[i] != null) {
-//                item[size] = items[i];
-//                size ++;
-//            }
-//        }
-//        return Arrays.copyOf(item, size);
-//    }
+
       public List<Item> findByName(String key){
         List<Item> item = new ArrayList<>();
         for(Item item1 : items) {
@@ -88,17 +57,7 @@ public final class Tracker {
         }
         return item;
       }
-//    public Item[] findByName(String key) {
-//         Item[] item = new Item[this.size];
-//        int size = 0;
-//        for (int i = 0; i < this.size; i++) {
-//            if (items[i].getName().equals(key)) {
-//                item[size] = items[i];
-//                size ++;
-//            }
-//        }
-//        return Arrays.copyOf(item, size);
-//    }
+
       public  boolean replace(int id, Item item) {
         boolean rsl = false;
         int index = indexOf(id);
@@ -108,16 +67,7 @@ public final class Tracker {
         }
         return rsl;
       }
-//    public boolean replace(int id, Item item) {
-//        boolean rsl = false;
-//     int index = indexOf(id);
-//     item.setId(id);
-//     if (index >= 0) {
-//         items[index] = item;
-//         rsl = true;
-//     }
-//     return rsl;
-//    }
+
       public boolean delete(int id) {
         boolean rsl = false;
         int index = indexOf(id);
@@ -127,16 +77,4 @@ public final class Tracker {
         }
         return rsl;
       }
-//    public boolean delete(int id) {
-//        boolean rsl = false;
-//        int index = indexOf(id);
-//        if (index != - 1) {
-//            items[index] = null;
-//            System.arraycopy(items, index + 1, items, index, size - index);
-//            items[size - 1] = null;
-//            size--;
-//            rsl = true;
-//        }
-//        return rsl;
-//    }
 }
