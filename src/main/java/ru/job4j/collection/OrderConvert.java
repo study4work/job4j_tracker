c1b3fb6bb92c00369647cf2af82053470e3807cc
study4work <FaJlJleN@ya.ru>
2021-04-27 21:04:24 +0300

    2. Преобразования List в Map.

--- src/main/java/ru/job4j/collection/OrderConvert.java
+++ src/main/java/ru/job4j/collection/OrderConvert.java
@@ -7,7 +7,9 @@ public class OrderConvert {
     public static HashMap<String, Order> process(List<Order> orders) {
         HashMap<String, Order> map = new HashMap<>();
         for (Order order : orders) {
-            map.put(order.getNumber(), order);
+            if (!map.containsKey(order.getNumber())) {
+                map.put(order.getNumber(), order);
+            }
         }
         return map;
     }
--- src/test/java/ru/job4j/collection/OrderConvertTest.java
+++ src/test/java/ru/job4j/collection/OrderConvertTest.java
@@ -24,6 +24,6 @@ public class OrderConvertTest {
         orders.add(new Order("3sfe", "Dress"));
         orders.add(new Order("3sfe", "Dress"));
         HashMap<String, Order> map = OrderConvert.process(orders);
-        assertThat(map.get("3sfe"), is(new Order("3sfe", "Dress")));
+        assertThat(map.size(), is(1));
     }
 }
