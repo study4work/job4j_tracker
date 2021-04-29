a6d2f312561f670d6c6393687246a06b1483e704
study4work <FaJlJleN@ya.ru>
2021-04-29 16:08:41 +0300

    4. Сортировка номера

---
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class LexSort implements Comparator<String> {
+
+    @Override
+    public int compare(String left, String right) {
+        String[] first = left.split(" ");
+        String[] second = right.split(" ");
+        double one = Double.parseDouble(first[0]);
+        double two = Double.parseDouble(second[0]);
+       return Double.compare(one,two);
+     }
+}
