5f30ba6c04fc96a71a4535ef811a194fcf8f3972
study4work <FaJlJleN@ya.ru>
2021-05-24 15:51:24 +0300

    1. Экзамен. Коллекции.

--- src/main/java/ru/job4j/collection/Article.java
+++ src/main/java/ru/job4j/collection/Article.java
@@ -1,30 +1,36 @@
 package ru.job4j.collection;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Article {
-    /**
-     *
+        /**
      * @param origin текст на вход
-     * @param line текст для проверки
+     * @param line   текст для проверки
      * @return true в случае,если line составлен из оригинального текста
-     * сравниваем попарно елементы из двух списков,в случае если они равны,прерываем второй цикл,
-     * для начала сравнения следующей пары и сохранения значения true при последней итерации
+     * добавляем в коллекцию элементы двух строк,результат основан на работе метода add,который
+         * возвращает false в случае если елемент уже присутствует в коллекции.
+         * В конце меняем местами результат работы метода.
      */
     public static boolean generateBy(String origin, String line) {
-        boolean result = false;
-        List<String> org = Arrays.asList(origin.split(" "));
-        List<String> ln = Arrays.asList(line.split(" "));
-        for (String first : org) {
-           for (String second : ln) {
-               result = first.contains(second);
-               if (result) {
-                   break;
-               }
-           }
+        boolean result = true;
+        Set<String> compare = new HashSet<>();
+        origin = origin.replaceAll("\\p{P}", "");
+        String[] org = origin.split(" ");
+        String[] ln = line.split(" ");
+
+        for (int i = 0; i < org.length; i++) {
+            compare.add(org[i]);
         }
+
+        for (int i = 0; i < ln.length; i++) {
+            result = compare.add(ln[i]);
+            if (result) {
+                break;
+            }
+        }
+       result = result == false ? true : false;
         return result;
+
     }
+
 }
