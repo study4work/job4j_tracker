8572f0f238873f8444ab8f8f00f2326016b7a6e2
study4work <FaJlJleN@ya.ru>
2021-05-27 17:05:56 +0300

    4. Зона видимости в лямбда-выражениях

---
+++ src/main/java/ru/job4j/function/ScopeInside.java
@@ -0,0 +1,21 @@
+package ru.job4j.function;
+
+import java.util.function.Supplier;
+
+public class ScopeInside {
+    public static void main(String[] args) {
+        int[] number = {1, 2, 3};
+        int total = 0;
+        for (int i = 0; i < number.length; i++) {
+            int num = number[i] + total;
+            total = add(
+                    () -> num
+            );
+        }
+        System.out.println(total);
+    }
+
+    private static Integer add(Supplier<Integer> calc) {
+        return calc.get();
+    }
+}
