928bc88781007ad4d11788cc926a1e64cfa883fd
study4work <FaJlJleN@ya.ru>
2021-05-31 22:53:50 +0300

    2. Список адресов

---
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ProfilesTest {
+    @Test
+    public void whenProfilesStramToAdress() {
+        Profiles pr = new Profiles();
+        Address adress = new Address("Moscow","Main",42,24);
+        Profile profile = new Profile(adress);
+        List<Profile> profiles = List.of(profile);
+        List<Address> expected = List.of(adress);
+        List<Address> rsl = pr.collect(profiles);
+        assertThat(rsl, is(expected));
+
+
+    }
+
+}
