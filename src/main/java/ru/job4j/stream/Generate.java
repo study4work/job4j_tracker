7b68f32e123be705cd177490bcfbe1a93099565b
study4work <FaJlJleN@ya.ru>
2021-06-10 21:06:54 +0300

    8. Перенос методов в Stream API

---
+++ src/main/java/ru/job4j/stream/Generate.java
@@ -0,0 +1,31 @@
+package ru.job4j.stream;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class Generate {
+    public enum Suit {
+        Diamonds, Hearts, Spades, Clubs
+    }
+
+    public enum Value {
+        V_6, V_7, V_8
+    }
+
+    public class Card {
+        private Suit suit;
+        private Value value;
+
+        public Card(Suit suit, Value value) {
+            this.suit = suit;
+            this.value = value;
+        }
+    }
+
+    public static void main(String[] args) {
+        Stream.of(Suit.values())
+                .flatMap(suit -> Stream.of(Value.values())
+                .map(value -> suit + " " + value))
+                .forEach(System.out::println);
+    }
+}
